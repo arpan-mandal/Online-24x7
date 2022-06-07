521344682e8254211d6351d1d156f8718dbf674a
Arpan-Mandal <92933926+Arpan-Mandal@users.noreply.github.com>
2022-06-07 10:22:26 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://apskyz.ga"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://cozmic.ga"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
